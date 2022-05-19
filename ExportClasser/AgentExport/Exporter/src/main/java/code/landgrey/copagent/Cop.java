c0cc81c183e1eb710972798d83f6f2a38a0ea87a
ffffliu <ffffliu@tencent.com>
2022-05-19 19:56:23 +0800

    update

--- ExportClasser/AgentExport/Exporter/src/main/java/code/landgrey/copagent/Cop.java
+++ ExportClasser/AgentExport/Exporter/src/main/java/code/landgrey/copagent/Cop.java
@@ -14,6 +14,7 @@ import java.util.*;
 public class Cop {
     public static File agent_work_directory = new File(PathUtils.getCurrentDirectory(), "dumpResult");
 
+    // 1、导出某些class会造成进程退出
     public static void main(String[] args) throws Exception {
         long pid = -1;
         String jvm_pid = null;
--- ShellChecker/src/main/java/Main.java
+++ ShellChecker/src/main/java/Main.java
@@ -10,7 +10,10 @@ import java.util.List;
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-
+        // java 7 环境运行的
+        // 1、通过检测import，以及superclass，但是很难，因为有多层继承
+        // 2、检测sink，反射
+        // 3、检测关键词，比如exploit
         Option zipOpt = new Option("d","directory",true,"class directory");
         zipOpt.setRequired(true);
         Options options = new Options();
